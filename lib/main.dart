9361f8e30943146fedc59d083779ac6b16da27ea
ZhangHe <zhangheorg@outlook.com>
2020-01-08 22:44:25 +0800

    feat(main.dart): hello, world

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    Center(
-      child: Text(
-        'Hello, world!',
-        textDirection: TextDirection.ltr,
-      ),
-    ),
-  );
+void main() => runApp(Bootsctrap());
+
+class Bootsctrap extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Hello, He",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Welcome to Flutter"),
+        ),
+        body: Center(
+          child: Text("Hello, world!"),
+        ),
+      )
+    );
+  }
 }
