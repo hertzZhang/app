16fa4c708d3c27bc6a2f38f3db9b2c1e19ea4a5c
ZhangHe <zhangheorg@outlook.com>
2020-01-08 15:24:52 +0800

    feat(pubspec.yaml): 增加http包

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:graphql_flutter/graphql_flutter.dart';
 
-void main() {
-  final HttpLink httpLink = HttpLink(uri: 'https://api.github.com/graphql');
+void main() => runApp(MaterialApp(
+      title: "Github with GraphQL",
+      debugShowCheckedModeBanner: false,
+      home: MyApp(),
+    ));
 
-  final AuthLink authLink = AuthLink(
-      getToken: () async => 'Bearer 03bbfeab805456dac6c3fad49d9b202817f3a5e0');
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
 
-  final Link link = authLink.concat(httpLink);
-
-  ValueNotifier<GraphQLClient> client = ValueNotifier(
-    GraphQLClient(
-      cache: InMemoryCache(),
-      link: link,
-    ),
-  );
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
 }
